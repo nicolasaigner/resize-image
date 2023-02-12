b6a25d94d2db5f0faefb43d27a4382d8a46a2bdc
Nícolas Aigner <17747420+nicolasaigner@users.noreply.github.com>
2023-02-11 21:10:11 -0300

    resize images

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ changeColorButton.addEventListener("click", () => {
     previewImage.src = canvas.toDataURL();
     previewImage.classList.add("preview-image");
     previewContainer.appendChild(previewImage);
-    previews.push(previewImage);
+
+
+    
+
+    previews.push(resizeImage(previewImage));
 
     downloadZipButton.disabled = false;
 
@@ -74,4 +78,34 @@ function saveAsZipFile(zip) {
       link.click();
       URL.revokeObjectURL(link.href);
   }
+}
+
+function resizeImage() {
+  // Create a new canvas for each size
+  const canvas72 = document.createElement("canvas");
+  canvas72.width = 72;
+  canvas72.height = 72;
+  const ctx72 = canvas72.getContext("2d");
+
+  const canvas36 = document.createElement("canvas");
+  canvas36.width = 36;
+  canvas36.height = 36;
+  const ctx36 = canvas36.getContext("2d");
+
+  const canvas18 = document.createElement("canvas");
+  canvas18.width = 18;
+  canvas18.height = 18;
+  const ctx18 = canvas18.getContext("2d");
+
+  // Draw the modified image on each canvas
+  ctx72.drawImage(canvas, 0, 0, canvas72.width, canvas72.height);
+  ctx36.drawImage(canvas, 0, 0, canvas36.width, canvas36.height);
+  ctx18.drawImage(canvas, 0, 0, canvas18.width, canvas18.height);
+
+  // Convert the canvas to a data URL
+  const dataURL72 = canvas72.toDataURL();
+  const dataURL36 = canvas36.toDataURL();
+  const dataURL18 = canvas18.toDataURL();
+
+  return [dataURL18, dataURL36, dataURL72].join(',');
 }
